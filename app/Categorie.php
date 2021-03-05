bfe6951b3a054d344313d3df94aa84dc36587443
zahokaci <zaho.6884@live.com>
2021-03-05 12:07:06 +0100

    modify models

--- app/Categorie.php
+++ app/Categorie.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
+    protected $guarded = [];
     public function products()
     {
         return $this->hasMany('App\Product');
--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $guarded = [];
     public function categorie()
     {
         return $this->belongsTo('App\Categorie');
